b67aff495aeba87ba58ab9df7b7f400744425952
ivan <ivanhernandezprincipal@gmail.com>
2019-05-28 18:24:16 +0200

    RangoEspada

---
+++ Assets/GameAssets/_Scripts/RangoDeDialogoDeLaEspada.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangoDeDialogoDeLaEspada : MonoBehaviour {
+
+    [SerializeField] float distancia = 2;
+    [SerializeField] CharacterController prota;
+    //cartel
+	
+	void Update () {
+	   if (Vector3.Distance(prota.transform.position, this.transform.position) < distancia)
+        {
+            Debug.Log("cerca");
+        }
+	}
+}
